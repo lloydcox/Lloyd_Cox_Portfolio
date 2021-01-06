3f1b2f3ead95b15a037a40ef4c8c077462d8f46c
Lloyd <lloydcox13@aol.com>
2021-01-06 15:18:09 +0000

    Update
    Removed some sections and chart, much simplier deisgn now.

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,11 +3,15 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-$('.imagtab').css("transition", "transform " + 0.02 * $('.imagtab').height() + "s ease");
+$(".imagtab").css("transition", "transform " + 1.2 * $(".imagtab").height() + "s ease");
 
-$('h1, h2').click(function() {
+$('h1, h2, h3').click(function() {
 	$(this).next('.section-text').toggle("hide");
 });
+
+// $('h3').click(function() {
+// 	$(this).next('.section-p').toggle("hide");
+// });
 
 });
 
