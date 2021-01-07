0ba86a759c5bbe18a4992a3bd8a32b6c83fdbf7c
Lloyd <lloydcox13@aol.com>
2021-01-07 15:25:43 +0000

    Update carousel bug fix
    Fixed the carousel bug fix. Minoe adjustments to other sections.

--- JS/scripts.js
+++ JS/scripts.js
@@ -11,13 +11,39 @@ $('h1, h2, h3').click(function() {
 
 $('.p-container').slick({
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-  });
-
-
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
 });
 
+})
 
