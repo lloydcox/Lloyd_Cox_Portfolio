c3c87f1435c27579f993e928c7e35b00e6fd42cd
Lloyd <lloydcox13@aol.com>
2021-01-26 20:51:56 +0000

    Major update
    Updated layout and design on small screen and large screen. Altered JS, added new styles and changed colour scheme. Whol re-design of the portfolio with most of the functionality as before and some new added.

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,76 +2,142 @@
 
 $(document).ready(function () {
   // Close/expand sections when clicked
-  $('.title-container, .inner-container ').click( function (event) {
-    event.stopPropagation();
-    if (!($(this).find('.section-text').is('.active')) && ($('.section-text').is('.active'))) {               
-        if ($('.text').is('.open')) {
-          $('div.text.open').toggleClass('hide open');
-          $('div.headings.active').toggleClass('active');
-          $('div.section-text.active').toggleClass('hide active');
-          $('div.inner-container.active').toggleClass('active');
-          $(this).find('.section-text').toggleClass('hide active');
-          $(this).closest('.inner-container').toggleClass('active');  
-      } else {
-          $('div.section-text.active').toggleClass('hide active');
-          $('div.inner-container.active').toggleClass('active');
-          $(this).find('.section-text').toggleClass('hide active');
-          $(this).closest('.inner-container').toggleClass('active'); 
-      }  
-    }
-    //  have to add else if including the headings not put the condtionals in the 4 else if already. 
-    else if ($(this).find('.section-text').is('.active'))  {
-          $(this).find('.section-text').toggleClass('hide active');
-          $(this).closest('.inner-container').toggleClass('active');
+//   $('.title-container, .inner-container ').click( function (event) {
+//     event.stopPropagation();
+//     if (!($(this).find('.section-text').is('.active-s')) && ($('.section-text').is('.active-s'))) {               
+//         if ($('.text').is('.open')) {
+//           $('div.text.open').toggleClass('hide open');
+//           $('div.headings.active-s').toggleClass('active-s');
+//           $('div.section-text.active-s').toggleClass('active-s');
+//           $('div.inner-container.active-s').toggleClass('active-s');
+//           $(this).find('.section-text').toggleClass('active-s');
+//           $(this).closest('.inner-container').toggleClass('active-s');  
+//       } else {
+//           $('div.section-text.active-s').toggleClass('active-s');
+//           $('div.inner-container.active-s').toggleClass('active-s');
+//           $(this).find('.section-text').toggleClass('active-s');
+//           $(this).closest('.inner-container').toggleClass('active-s'); 
+//       }  
+//     }
+//     //  have to add else if including the headings not put the condtionals in the 4 else if already. 
+//     else if ($(this).find('.section-text').is('.active-s'))  {
+//           $(this).find('.section-text').toggleClass('active-s');
+//           $(this).closest('.inner-container').toggleClass('active-s');
         
-    }
+//     }
 
-    else if ($('.section-text').is('.active')) {
-        if ($('.text').is('.open')) {
-          $('.text').toggleClass('hide open');
-          $('div.section-text.active').toggleClass('hide active');
-          $('div.inner-container.active').toggleClass('active');
-          $(this).find('.section-text').toggleClass('hide active');
-          $(this).closest('.inner-container').toggleClass('active');  
-      } else {
-          $('div.section-text.active').toggleClass('hide active');
-          $('div.inner-container.active').toggleClass('active');
-          $(this).find('.section-text').toggleClass('hide active');
-          $(this).closest('.inner-container').toggleClass('active'); 
-      } 
-    } 
-    else if (!($(this).find('.section-text').is('.active')))  {
-          $(this).find('.section-text').toggleClass('hide active');
-          $(this).closest('.inner-container').toggleClass('active'); 
-    } 
-})
+//     else if ($('.section-text').is('.active-s')) {
+//         if ($('.text').is('.open')) {
+//           $('.text').toggleClass('hide open');
+//           $('div.section-text.active-s').toggleClass('active-s');
+//           $('div.inner-container.active-s').toggleClass('active-s');
+//           $(this).find('.section-text').toggleClass('active-s');
+//           $(this).closest('.inner-container').toggleClass('active-s');  
+//       } else {
+//           $('div.section-text.active-s').toggleClass('active-s');
+//           $('div.inner-container.active-s').toggleClass('active-s');
+//           $(this).find('.section-text').toggleClass('active-s');
+//           $(this).closest('.inner-container').toggleClass('active-s'); 
+//       } 
+//     } 
+//     else if (!($(this).find('.section-text').is('.active-s')))  {
+//           $(this).find('.section-text').toggleClass('active-s');
+//           $(this).closest('.inner-container').toggleClass('active-s'); 
+//     } 
+// })
 
-  $('.headings').click(function (event) {
-    event.stopPropagation();
-    if (!($(this).is('.active')) && ($('.headings').is('.active')) )  {
-      $('div.headings.active').toggleClass('active');
+  $('.about-me-heading').click(function (event) {
+    event.stopPropagation();  
+    if (!($(this).is('.active-s')) && ($('.about-me-heading').is('.active-s')) )  {
+      $('div.about-me-heading.active-s').toggleClass('active-s');
       $('div.text.open').toggleClass('hide open');      
-      $(this).toggleClass('active');
+      $(this).toggleClass('active-s');
       $(this).find('.text').toggleClass('hide open'); 
     }
 
-    else if ($(this).is('.active'))  {
-      $(this).toggleClass('active');
+    else if ($(this).is('.active-s'))  {
+      $(this).toggleClass('active-s');
       $(this).find('.text').toggleClass('hide open');
     }
 
-    else if ($('.headings').is('.active')) {
-      $('div.headings.active').toggleClass('active');
+    else if ($('.about-me-heading').is('.active-s')) {
+      $('div.about-me-heading.active-s').toggleClass('active-s');
       $('div.text.open').toggleClass('hide open');
-      $(this).toggleClass('active');
+      $(this).toggleClass('active-s');
       $(this).find('.text').toggleClass('hide open');  
     } 
 
-    else if (!($(this).is('.active')))  {
-      $(this).toggleClass('active');
+    else if (!($(this).is('.active-s')))  {
+      $(this).toggleClass('active-s');
       $(this).find('.text').toggleClass('hide open'); 
     } 
   });
+
+  $('.work').click(function (event) {
+    if ($('.welcome').is('.on')) {
+    $('.welcome').toggleClass('hide on');
+    $('.work-text').toggleClass('hide on');
+    $('.work').toggleClass('hide');
+    $('.intro').toggleClass('hide');
+    } else if ($('.personal-text').is('.on')) {
+      $('.personal-text').toggleClass('hide on');
+      $('.personal').toggleClass('hide');
+      $('.work-text').toggleClass('hide on');
+      $('.work').toggleClass('hide');
+    }
+  })
+
+  $('.personal').click(function (event) {
+    if ($('.welcome').is('.on')) {
+    $('.welcome').toggleClass('hide on');
+    $('.intro').toggleClass('hide');
+    $('.personal-text').toggleClass('hide on');
+    $('.personal').toggleClass('hide');
+    } else if ($('.work-text').is('.on')) {
+      $('.work-text').toggleClass('hide on');
+      $('.work').toggleClass('hide');
+      $('.personal-text').toggleClass('hide on');
+      $('.personal').toggleClass('hide');
+    }
+  })
+
+  $('.intro').click(function (event) {
+    if ($('.work-text').is('.on')) {
+    $('.work-text').toggleClass('hide on');
+    $('.work').toggleClass('hide');
+    $('.welcome').toggleClass('hide on');
+    $('.intro').toggleClass('hide');
+    } else if ($('.personal-text').is('.on')) {
+      $('.personal-text').toggleClass('hide on');
+      $('.personal').toggleClass('hide');
+      $('.welcome').toggleClass('hide on');
+      $('.intro').toggleClass('hide');
+
+    }
+  })
+
+
+
+  // $('.about-me-heading').click(function (event) {
+  //   event.stopPropagation();
+  //   if (!($(this).is('.wide')) && ($('.headings').is('.wide')) )  {
+  //     $('div.headings.wide').toggleClass('wide');   
+  //     $(this).toggleClass('wide');
+  //   }
+
+  //   else if ($(this).is('.wide'))  {
+  //     $(this).toggleClass('wide');
+  //   }
+
+  //   else if ($('.headings').is('.wide')) {
+  //     $('div.headings.wide').toggleClass('wide');
+  //     $(this).toggleClass('wide');
+  //   } 
+
+  //   else if (!($(this).is('.wide')))  {
+  //     $(this).toggleClass('wide');
+  //   } 
+  // });
 
   // show details or video when clicking button in carousel
   $('.show').click(function (event) {
@@ -80,10 +146,7 @@ $(document).ready(function () {
     $('.project-text').toggle('hide')
     $('.show').toggleClass('hide')
   })
-  // show hide carousel when title clicked
-  // $('.projects-t').click(function () {
-  //   $('.p-container').toggle('hide')
-  // })
+
 
   // carousel settings
   $('.main-slider').slick({
@@ -91,10 +154,12 @@ $(document).ready(function () {
     speed: 400,
     slidesToShow: 1,
     adaptiveHeight: true,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 3000,
-    pauseOnFocus: true
+    pauseOnFocus: true,
+    dots: true
   })
+ 
 })
 
   
