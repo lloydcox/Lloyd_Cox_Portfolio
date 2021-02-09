5ae6869ec044ce0a3f0e864146784ed51d232fea
Lloyd <lloydcox13@aol.com>
2021-02-09 17:52:27 +0000

    Major bug fix and update
    Fixed some bugs and updated layout/design. One more push needed to complete first phase.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,137 @@
 // Scripts
 
 $(document).ready(function () {
+
+  $(window).load(function() {
+    if ($(window).width() >= 768) {
+          $('.box-text').addClass('textBox');
+    }
+  });
+
+  $(window).load(function() {
+    if ($(window).width() < 990)  {
+      $('.about-me-heading').addClass('toggs');
+      $('.toggs').bind('click', function (event) {
+        event.stopPropagation();  
+        if (!($(this).is('.active-s')) && ($('.about-me-heading').is('.active-s')) )  {
+          $('div.about-me-heading.active-s').toggleClass('active-s');
+          $('div.text.open').toggleClass('hide textBox open');      
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open');           
+        }
+    
+        else if ($(this).is('.active-s'))  {
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open');
+        }
+    
+        else if ($('.about-me-heading').is('.active-s')) {
+          $('div.about-me-heading.active-s').toggleClass('active-s');
+          $('div.text.open').toggleClass('hide textBox open');
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open');  
+        } 
+    
+        else if (!($(this).is('.active-s')))  {
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open'); 
+        } 
+      });
+    }   if ($(window).width() >= 990) {
+          $('.work-text').removeClass('hide');
+          $('.about-me-heading').addClass('high');
+          $('.about-me-heading').removeClass('toggs');
+          $('.work-text').addClass('textBox');
+    }
+  });
+
+  $(window).resize(function () {
+    if (($(window).width() < 990) && ($('.small-screen').is('.hide'))) {
+      $(".small-screen").removeClass("hide");
+      $(".large-screen").addClass("hide");
+      $('.design').toggleClass("hide");
+      $('.header-text').toggleClass("hide"); 
+      $('.toggs').bind('click', function (event) {
+        event.stopPropagation();  
+        if (!($(this).is('.active-s')) && ($('.about-me-heading').is('.active-s')) )  {
+          $('div.about-me-heading.active-s').toggleClass('active-s');
+          $('div.text.open').toggleClass('hide textBox open');      
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open'); 
+        }
+    
+        else if ($(this).is('.active-s'))  {
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open');
+        }
+    
+        else if ($('.about-me-heading').is('.active-s')) {
+          $('div.about-me-heading.active-s').toggleClass('active-s');
+          $('div.text.open').toggleClass('hide textBox open');
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open');  
+        } 
+    
+        else if (!($(this).is('.active-s')))  {
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open'); 
+        } 
+        });
+    }
+    else if (($(window).width() < 990)) {
+      $('.work-text').addClass('hide');
+      $('.text').removeClass('textBox open');
+      $('.about-me-heading').removeClass('high active-s');
+      $('.about-me-heading').addClass('toggs')
+      $('.toggs').bind('click', function (event) {
+        event.stopPropagation();  
+        if (!($(this).is('.active-s')) && ($('.about-me-heading').is('.active-s')) )  {
+          $('div.about-me-heading.active-s').removeClass('active-s');
+          $('div.text.open').removeClass('hide textBox open');      
+          $(this).addClass('active-s');
+          $(this).find('.text').addClass('textBox'); 
+          $(this).find('.text').removeClass('hide open'); 
+        }
+    
+        else if ($(this).is('.active-s'))  {
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open');
+        }
+    
+        else if ($('.about-me-heading').is('.active-s')) {
+          $('div.about-me-heading.active-s').toggleClass('active-s');
+          $('div.text.open').toggleClass('hide textBox open');
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open');  
+        } 
+    
+        else if (!($(this).is('.active-s')))  {
+          $(this).toggleClass('active-s');
+          $(this).find('.text').toggleClass('hide textBox open'); 
+        }
+      });
+    }    
+  });
+
+  $(window).resize(function () {
+    if ($(window).width() > 990) {
+      $('.work-text').removeClass('hide');
+      $('.about-me-heading').addClass('high');
+      $('.work-text').addClass('textBox');
+      $('.about-me-heading').removeClass('toggs');
+    }
+  });
    
   $('.work-s').click(function (event) {
     if ($('.welcome').is('.on')) {
     $('.welcome').toggleClass('hide on');
+    $('.intro').toggleClass('hide');
     $('.work-text-s').toggleClass('hide on');
     $('.work-s').toggleClass('hide');
-    $('.intro').toggleClass('hide');
     $('.head-s').addClass('hide');
-    $('.holding').addClass('hold-height');
+    $('.holding').addClass('hold-height rows');
+    $('.select-buttons').addClass('rows');
+    $('.intro, .personal').addClass('button-size');
     } else if ($('.personal-text').is('.on')) {
       $('.personal-text').toggleClass('hide on');
       $('.personal').toggleClass('hide');
@@ -17,6 +139,8 @@ $(document).ready(function () {
       $('.work-s').toggleClass('hide');
       $('.head-s').addClass('hide');
       $('.holding').addClass('hold-height');
+      $('.select-buttons').addClass('rows');
+      $('.intro, .personal').addClass('button-size');
     }
   });
 
@@ -28,13 +152,17 @@ $(document).ready(function () {
     $('.personal').toggleClass('hide');
     $('.head-s').addClass('hide');
     $('.holding').addClass('hold-height');
-    } else if ($('.work-text').is('.on')) {
+    $('.select-buttons').addClass('rows');
+    $('.intro, .work-s').addClass('button-size');
+    } else if ($('.work-text-s').is('.on')) {
       $('.work-text-s').toggleClass('hide on');
       $('.work-s').toggleClass('hide');
       $('.personal-text').toggleClass('hide on');
       $('.personal').toggleClass('hide');
       $('.head-s').addClass('hide');
       $('.holding').addClass('hold-height');
+      $('.select-buttons').addClass('rows');
+      $('.intro, .work-s').addClass('button-size');
     }
   });
 
@@ -46,6 +174,8 @@ $(document).ready(function () {
     $('.intro').toggleClass('hide');
     $('.head-s').removeClass('hide');
     $('.holding').removeClass('hold-height');
+    $('.select-buttons').removeClass('rows');
+    $('.personal, .work-s').removeClass('button-size');
     } else if ($('.personal-text').is('.on')) {
       $('.personal-text').toggleClass('hide on');
       $('.personal').toggleClass('hide');
@@ -53,6 +183,8 @@ $(document).ready(function () {
       $('.intro').toggleClass('hide');
       $('.head-s').removeClass('hide');
       $('.holding').removeClass('hold-height');
+      $('.select-buttons').removeClass('rows');
+      $('.personal, .work-s').removeClass('button-size');
     }
   });
 
@@ -91,115 +223,8 @@ $(document).ready(function () {
     dots: true
   });
 
-  $(window).load(function() {
-    if ($(window).width() < 990)  {
-      $('.about-me-heading').addClass('toggs');
-      $('.toggs').click(function (event) {
-        event.stopPropagation();  
-        if (!($(this).is('.active-s')) && ($('.about-me-heading').is('.active-s')) )  {
-          $('div.about-me-heading.active-s').toggleClass('active-s');
-          $('div.text.open').toggleClass('hide open');      
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open'); 
-        }
-    
-        else if ($(this).is('.active-s'))  {
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open');
-        }
-    
-        else if ($('.about-me-heading').is('.active-s')) {
-          $('div.about-me-heading.active-s').toggleClass('active-s');
-          $('div.text.open').toggleClass('hide open');
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open');  
-        } 
-    
-        else if (!($(this).is('.active-s')))  {
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open'); 
-        } 
-      });
-    }   if ($(window).width() >= 990) {
-          $('.work-text').removeClass('hide');
-          $('.about-me-heading').addClass('high');
-          $('.about-me-heading').removeClass('toggs');
-    }
-  });
 
-  $(window).resize(function () {
-    if (($(window).width() < 990) && ($('.small-screen').is('.hide'))) {
-      $(".small-screen").removeClass("hide");
-      $(".large-screen").addClass("hide");
-      $('.design').toggleClass("hide");
-      $('.header-text').toggleClass("hide"); 
-      $('.toggs').click(function (event) {
-        event.stopPropagation();  
-        if (!($(this).is('.active-s')) && ($('.about-me-heading').is('.active-s')) )  {
-          $('div.about-me-heading.active-s').toggleClass('active-s');
-          $('div.text.open').toggleClass('hide open');      
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open'); 
-        }
-    
-        else if ($(this).is('.active-s'))  {
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open');
-        }
-    
-        else if ($('.about-me-heading').is('.active-s')) {
-          $('div.about-me-heading.active-s').toggleClass('active-s');
-          $('div.text.open').toggleClass('hide open');
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open');  
-        } 
-    
-        else if (!($(this).is('.active-s')))  {
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open'); 
-        } 
-        });
-    }
-    else if (($(window).width() < 990)) {
-      $('.work-text').addClass('hide');
-      $('.about-me-heading').removeClass('high active-s');
-      $('.about-me-heading').addClass('toggs')
-      $('.toggs').click(function (event) {
-        event.stopPropagation();  
-        if (!($(this).is('.active-s')) && ($('.about-me-heading').is('.active-s')) )  {
-          $('div.about-me-heading.active-s').toggleClass('active-s');
-          $('div.text.open').toggleClass('hide open');      
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open'); 
-        }
-    
-        else if ($(this).is('.active-s'))  {
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open');
-        }
-    
-        else if ($('.about-me-heading').is('.active-s')) {
-          $('div.about-me-heading.active-s').toggleClass('active-s');
-          $('div.text.open').toggleClass('hide open');
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open');  
-        } 
-    
-        else if (!($(this).is('.active-s')))  {
-          $(this).toggleClass('active-s');
-          $(this).find('.text').toggleClass('hide open'); 
-        } 
-      });
-    }    
-  });
 
-  $(window).resize(function () {
-    if ($(window).width() > 990) {
-      $('.work-text').removeClass('hide');
-      $('.about-me-heading').addClass('high');
-      $('.about-me-heading').removeClass('toggs');
-    }
-  });
 
 });
 
